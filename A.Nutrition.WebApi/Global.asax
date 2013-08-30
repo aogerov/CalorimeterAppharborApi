f3b4c9163c226dbd5bf4257c8db7fa587b63cf92
antonios123 <otr@abv.bg>
2013-08-30 16:21:29 +0300

    Calorimeter Appharbor Api

---
+++ A.Nutrition.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="A.Nutrition.WebApi.WebApiApplication" Language="C#" %>
---
+++ A.Nutrition.WebApi/Models/ProductsByKeywordModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using A.Nutrition.WebApi.Models;
+
+namespace A.Nutrition.WebApi.Models
+{
+    [DataContract]
+    public class ProductsByKeywordModel
+    {
+        public ProductsByKeywordModel()
+        {
+            this.Products = new HashSet<ProductModel>();
+        }
+
+        [DataMember(Name = "id")]
+        public int Id { get; set; }
+
+        [DataMember(Name = "keyword")]
+        public string Keyword { get; set; }
+
+        [DataMember(Name = "products")]
+        public ICollection<A.Nutrition.WebApi.Models.ProductModel> Products { get; set; }
+    }
+}
